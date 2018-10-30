086339b4b2b05600b6d1425a3b61e87badc8f3d5
Leonardo <liogothicvampire@gmail.com>
2018-10-30 17:50:27 -0600

    create the Maven Project

---
+++ pdfreader/src/main/java/com/liobouchan/pdfreader/App.java
@@ -0,0 +1,13 @@
+package com.liobouchan.pdfreader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
