64590e7a3860152d4818c5bb3fc298a092f6892a
Leonardo <liogothicvampire@gmail.com>
2018-10-30 18:07:55 -0600

    Implementation of the Library

--- pdfreader/src/main/java/com/liobouchan/pdfreader/App.java
+++ pdfreader/src/main/java/com/liobouchan/pdfreader/App.java
@@ -1,5 +1,8 @@
 package com.liobouchan.pdfreader;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        LineIterator it = FileUtils.lineIterator(file,"UTF-8");
+        try {
+        	while (it.hasNext()) {
+        		String line = it.nextLine();
+        		// hacer cosas
+			}
+		} finally {
+			// TODO: handle finally clause
+			LineIterator.closeQuietly(it);
+		}
     }
 }
